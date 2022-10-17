77d369fe87d303307f679dd0580050b07cad00d7
Troy Shilton <S00221754@atu.ie>
2022-10-17 10:29:53 +0100

    Q3 done

--- Assessment1Q3/Program.cs
+++ Assessment1Q3/Program.cs
@@ -14,7 +14,33 @@ namespace Assessment1Q3
     {
         static void Main(string[] args)
         {
+            string results = "WDDLWWDLWW";
+            int totalPoints = 0, wins = 0, draws = 0, losses = 0;
 
+            for (int i = 0; i < 10; i++)
+            {
+                char result = results[i];
+
+                if(result == 'W')
+                {
+                    totalPoints = totalPoints + 3;
+                    wins++;
+                }
+                else if(result == 'D')
+                {
+                    totalPoints++;
+                    draws++;
+                }
+                else if (result == 'L')
+                {
+                    losses++;
+                }
+            }
+            Console.WriteLine($"{wins} wins");
+            Console.WriteLine($"{draws} draws");
+            Console.WriteLine($"{losses} losses");
+            Console.WriteLine($"Total points: {totalPoints}");
+            Console.ReadLine();
         }
     }
 }
